2bac8f1bb641a423cba7abcb301446d50893a2a8
Anna Yannakopoulos <anna.yannakopoulos@gmail.com>
2019-04-17 19:20:26 -0400

    Return embedding indices instead of tokens

--- src/babi.py
+++ src/babi.py
@@ -17,7 +17,7 @@ def tokenize(line):
     tokens : list(str)
         List of tokens in line.
     """
-    tokens = [x.strip() for x in re.split("(\W+)+", line) if x.strip()]
+    tokens = [x.strip().lower() for x in re.split("(\W+)+", line) if x.strip()]
     return tokens
 
 
@@ -36,10 +36,15 @@ def load_data(path, tasks=None):
 
     Returns
     -------
-    data : list(tuple(list(list(str)), list(str), str))
-        A list of story, question, answer tuples. The story is a list of
-        statements and each statement is itself a list of tokens. The question
-        is similarly a list of tokens. The answer is a single token.
+    stories : list(list(list(str)))
+        A list of stories. Each story is a list of statements and each
+        statement is a list of tokens.
+    questions : list(list(str))
+        A list of questions corresponding to each story. Each question is a
+        list of tokens.
+    answers : list(str)
+        A list of answers corresponding to each story and question. Each answer
+        is a single word.
     """
 
     if tasks is None:
@@ -49,7 +54,9 @@ def load_data(path, tasks=None):
             os.path.join(path, file) for file in os.listdir(path)
             if int(re.search(r"qa(\d+)", file).group(1)) in tasks]
 
-    data = []
+    stories = []
+    questions = []
+    answers = []
 
     for file in files:
         with open(file, "r") as f:
@@ -66,9 +73,68 @@ def load_data(path, tasks=None):
                     substory = [
                         ["{}:".format(i + 1)] + line
                         for i, line in enumerate(story)]
-                    data.append((substory, question, answer))
+                    stories.append(substory)
+                    questions.append(question)
+                    answers.append(answer)
                 else:
                     # regular line
                     story.append(tokenize(line))
 
-    return data
+    return stories, questions, answers
+
+
+def data_to_index(data, dictionary):
+    stories, questions, answers = data
+    stories = [[[
+        dictionary.get(word, 0) for word in statement]
+        for statement in story]
+        for story in stories]
+    questions = [
+        [dictionary.get(word, 0) for word in question]
+        for question in questions]
+    answers = [dictionary.get(answer, 0) for answer in answers]
+    return stories, questions, answers
+
+
+def load_corpus(path, tasks=None):
+    """
+    Loads bAbI corpus.
+
+    Parameters
+    ----------
+    path : str
+        Path to folder containing bAbI corpus files of the form
+        `qa{task}_train.txt`
+    tasks : list(int) or "all"
+        Task(s) to load training data from. If task(s) not given, loads all
+        available tasks.
+
+    Returns
+    -------
+    train_data : tuple(list(list(list(int))), list(list(int)), list(int))
+        Training data tuple of stories, questions, and answers. Each story is a
+        list of statements and each statement is a list of ints representing
+        the embedding index of a token. Similarly, each question is a list of
+        ints representing the embedding index of a token. Each answer is a
+        single int representing the embedding index of a one-word answer. The 0
+        index is reserved for a padding embedding.
+    test_data : tuple(list(list(list(int))), list(list(int)), list(int))
+        Same as above, but for testing data.
+    n_words : int
+        Number of unique tokens in the training corpus.
+    """
+    train_data = load_data(os.path.join(path, "train"), tasks=tasks)
+    test_data = load_data(os.path.join(path, "test"), tasks=tasks)
+
+    vocab = set([
+        word for story in train_data[0]
+        for statement in story
+        for word in statement])
+    vocab.update([word for question in train_data[1] for word in question])
+    dictionary = {word: i + 1 for i, word in enumerate(vocab)}
+    n_words = len(vocab)
+
+    train_data = data_to_index(train_data, dictionary)
+    test_data = data_to_index(test_data, dictionary)
+
+    return train_data, test_data, n_words
